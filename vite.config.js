d2cf475141e655e0a37b79f72a2903c496129068
AkshanviC <vigneshakshan@gmail.com>
2024-06-14 15:59:24 +0530

    config update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://akshanvic.github.io/movieSearchandPaginate/",
+  base: "/movieSearchandPaginate/",
 });
